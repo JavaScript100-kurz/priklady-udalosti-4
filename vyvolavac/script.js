088a0e4e8bd227f0e26b1371fd225dc1366f475c
Luděk Roleček <ludek@rolecek.cz>
2023-07-04 17:46:57 +0200

    Aktualizace podkladů

---
+++ vyvolavac/script.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/*
+Máš pole jmen - je to seznam žáků ve třídě.
+Napiš program, který bude žáky náhodně vyvolávat k tabuli.
+
+Po stisknutí tlačítka na stránce:
+
+1) vyber náhodně jedno jméno ze seznamu
+
+2) vybrané jména napiš dovnitř prvku s id="vyvolany"
+
+3) V programu si založ pole, do kterého budeš ukládat jména
+již vyvolaných žáků. Obsah tohoto pole vypisuj na stránce
+do prvku s id="seznam".
+
+Při každém vyvolání nového žáka (tj. po stisku tlačítka)
+do seznam vyvolaných přidej to nové, náhodně vybrané jméno.
+
+BONUS:
+4) Zařid, aby žák nemohl být vyvolaný dvakrát.
+*/
+
+
+const jmena = [
+  "Hana",
+  "Tereza",
+  "Jana",
+  "Iveta",
+  "Natália",
+  "Lucie",
+  "Adéla ",
+  "Lenka",
+  "Beáta",
+  "Eva ",
+  "Michaela",
+  "Alžbeta",
+  "Alexandra",
+  "Petra",
+  "Kateřina",
+  "Honza"
+];
+
+
---
+++ zarovka/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/*
+V HTML je prvek s id="zarovka".
+
+Žárovku můžeš rozsvítit tak, že na ní přidáš CSS třídu "zarovka--sviti".
+
+Při stisku klávesy MEZERA žárovku zhasni nebo rozsviť.
+Při stisku klávesy R žárovku vždy rozsviť (ať je v jakémkoliv stavu).
+Při stisku klávesy Z žárovku vždy zhasni.
+*/
